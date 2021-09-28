64564df4127d887c15ba6ef55f3e4283e0bc2c79
Antonipo <lui.rodriguez.antonio@gmail.com>
2021-09-28 15:15:54 -0500

    sube!

---
+++ prueba/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'My Library',
+    'depends': ['base','sale'],
+    'data': ['views/save_links.xml'],
+
+ }
---
+++ prueba/models/library_book.py
@@ -0,0 +1,29 @@
+from odoo import models,fields
+
+
+class LibraryBook(models.Model):
+    _name ='library.book'
+    name = fields.Char('Title', required=True)
+    date_release = fields.Date('Release Date')
+    publisher_id = fields.Many2one(
+        'res.partner', string='Publisher',
+        # optional:
+        ondelete='set null',
+        context={},
+        domain=[],
+    )
+    author_ids = fields.Many2many(
+        'res.partner', string='Authors')
+
+class ResPartner(models.Model):
+     _inherit = 'res.partner'
+     published_book_ids = fields.One2many(
+     'library.book', 'publisher_id',
+     string='Published Books')
+     authored_book_ids = fields.Many2many(
+     'library.book',
+     string='Authored Books',
+     # relation='library_book_res_partner_rel' #
+     )
+
+
