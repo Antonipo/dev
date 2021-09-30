ff705ddcb0ec5258c02cb89078d56f1d321410b7
Antonipo <lui.rodriguez.antonio@gmail.com>
2021-09-30 18:59:01 -0500

    chapter 4.2

--- chapter_4/models/library_book.py
+++ chapter_4/models/library_book.py
@@ -14,9 +14,30 @@ class LibraryBook(models.Model):
      _rec_name = 'short_name'
      short_name = fields.Char('Short Title' , required = True)
 
+    #this is for the page tag
      def name_get(self):
          result = []
          for record in self:
              rec_name = "%s (%s)" % (record.name,record.date_release)
              result.append((record.id,rec_name))
              return result
+
+     #adding data fields to a model
+     notes = fields.Text('Internal Notes')
+     state = fields.Selection(
+             [
+                 ('draft', 'Not Avalible'),
+                 ('avalible','Avalible'),
+                 ('lost','Lost'),
+             ],'State'
+         )
+     descriptions = fields.Html('Descriptions')
+     cover = fields.Boolean('Book Cover')
+     out_of_print = fields.Boolean('Out of Print?')
+     date_updated = fields.Datetime('Last Updated')
+     pages = fields.Integer('Number of Pages')
+     reader_rating = fields.Float(
+         'Reader Average Rating',
+         digits =(14,4) ,#Precision decimal
+
+     )
