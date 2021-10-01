4069872b95b59d08dc6356ded1ccc66c84232694
Antonipo <lui.rodriguez.antonio@gmail.com>
2021-10-01 12:49:26 -0500

    chapter 4.5

--- chapter_4/models/library_book.py
+++ chapter_4/models/library_book.py
@@ -4,10 +4,7 @@ class LibraryBook(models.Model):
      _name = 'library.book.2'
      name = fields.Char('Title', required=True)
      date_release = fields.Date('Release Date')
-     author_ids = fields.Many2many(
-         'res.partner',
-         string='Authors'
-        )
+
      #extra model definitions
      _descriptions = 'Library book'
      _order = 'date_release desc, name'
@@ -32,12 +29,44 @@ class LibraryBook(models.Model):
              ],'State'
          )
      descriptions = fields.Html('Descriptions')
-     cover = fields.Boolean('Book Cover')
+     cover = fields.Binary('Book Cover')
      out_of_print = fields.Boolean('Out of Print?')
      date_updated = fields.Datetime('Last Updated')
      pages = fields.Integer('Number of Pages')
      reader_rating = fields.Float(
          'Reader Average Rating',
          digits =(14,4) ,#Precision decimal
-
      )
+
+     # setting float field precision with use of odoo property
+     cost_price=fields.Float('Book Cost',digits='Book Price')
+
+     #adding Monetary field
+     currency_id = fields.Many2one('res.currency', String ='Currency')
+     retail_price = fields.Monetary('Retail Price',
+                                    #optional :Currency_field = 'currency_id'
+                                    Currency_field='currency_id'
+                                    )
+     #adding relational fields to a model
+     publisher_id = fields.Many2one(
+         'res.partner', string='Publisher',
+         # optional:
+         ondelete='set null',
+         context={},
+         domain=[],
+     )
+     author_ids = fields.Many2many(
+         'res.partner', string='Authors')
+
+#the model class for the relational fields
+class ResPartner(models.Model):
+     _inherit = 'res.partner'
+     published_book_ids = fields.One2many(
+         'library.book.2', 'publisher_id',
+         string='Published Books')
+     authored_book_ids = fields.Many2many(
+         'library.book.2',
+         string='Authored Books',
+         # relation='library_book_res_partner_rel' # optional
+     )
+
